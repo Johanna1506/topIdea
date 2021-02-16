3caed1f4343bed565f6c3e0af85689bdf3dd90db
Johanna Jato <johanna.jato@gmail.com>
2021-02-16 16:34:37 +0100

    entity idea

---
+++ src/main/java/fr/epsi/TopIdea/entity/Idea.java
@@ -0,0 +1,71 @@
+package fr.epsi.TopIdea.entity;
+
+
+import javax.persistence.*;
+import java.lang.reflect.Array;
+
+@Entity
+public class Idea {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String date;
+
+    private String title;
+
+    private String tag;
+
+    private String description;
+
+    private String image;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+}
---
+++ src/main/webapp/pages/commons/nav.jsp
@@ -0,0 +1,10 @@
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<header>
+    <nav>
+        <ul>
+            <li>TOP IDEA</li>
+            <li>Voir les idées</li>
+            <li>Soumettre une idée</li>
+        </ul>
+    </nav>
+</header>
