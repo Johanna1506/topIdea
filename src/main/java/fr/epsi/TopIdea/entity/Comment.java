450fd8b4f3d9b297584d5dbaa2f3261d220800bd
Ca-Jou <camille.pauron@gmail.com>
2021-02-16 16:33:50 +0100

    User, Comment, Vote entities

---
+++ src/main/java/fr/epsi/TopIdea/entity/Comment.java
@@ -0,0 +1,51 @@
+package fr.epsi.TopIdea.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private User user;
+    private Idea idea;
+    private String text;
+
+    // getters & setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Idea getIdea() {
+        return idea;
+    }
+
+    public void setIdea(Idea idea) {
+        this.idea = idea;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
