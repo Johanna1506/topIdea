ee41cc75e00f2fc6ac24e601c7a3e70d1fbd16c3
Ca-Jou <camille.pauron@gmail.com>
2021-02-18 19:03:20 +0100

    add SQL file

---
+++ 0-DB/Dummies.sql
@@ -0,0 +1,22 @@
+INSERT INTO Role VALUES
+	(1, 'Chef.fe de la Terre'),
+	(2, 'Grosse Pompe');
+
+INSERT INTO User (id, mail, password, username, role_id) VALUES
+	(1, 'johanna.jato@epsi.fr', 'johanna', 'johanna', 2),
+	(2, 'camille,jouan@epsi.fr', 'camille', 'camille', 1),
+	(3, 'donovan.damhec@epsi.fr', 'donovan', 'donovan', 1),
+	(4, 'claire.lapompe@epsi.fr', 'claire', 'claire', 2),
+	(5, 'toto@epsi.fr', 'toto', 'toto', 2);
+
+INSERT INTO Category VALUES
+	(1, 'Revolution'),
+	(2, 'Connasse'),
+	(3, 'Rouleur patin');
+
+INSERT INTO Idea (date, description, image, title, author_id, category_id) VALUES
+	(2021-01-01, 'Elisons Johanna a la presidence de la planete, ca tombe sous le sens.', 'https://www.groupe-imprim.com/wp-content/uploads/2019/09/election-img2.jpg', 'Johanna Presidente', 2, 1),
+	(2021-02-16, 'Les patins ca coute la blinde. On en offre une paire a tout le monde!!!', 'https://pbs.twimg.com/media/DlwkRQsU4AAa4tU.jpg', 'Patins gratuits', 3, 3),
+	(2021-02-02, 'Les rendus par mail ca me degoute!', 'https://resize-parismatch.lanmedia.fr/r/620,/img/var/news/storage/images/media/images/super-nanny-portrait/207874-1-fre-FR/Super-Nanny-portrait_original_backup.jpg', '1 point de penalite', 4, 2);
+
+	
