ef533cc4ab0f059825343c43d8fe73a162a8cfb3
Ca-Jou <camille.pauron@gmail.com>
2021-04-10 19:54:28 +0200

    update data set

--- 0-DB/Database.sql
+++ 0-DB/Database.sql
@@ -73,8 +73,8 @@ INSERT INTO User (id, username, password, isActive) VALUES
 	(1, 'johanna', 'johanna', TRUE),
 	(2, 'camille', 'camille', TRUE),
 	(3, 'donovan', 'donovan', TRUE),
-	(4, 'claire', 'claire', TRUE),
-	(5, 'toto', 'toto', TRUE);
+	(4, 'laurent', 'laurent', TRUE),
+	(5, 'leon', 'leon', TRUE);
 
 INSERT INTO UserRole VALUES
     (1, 1),
@@ -87,13 +87,13 @@ INSERT INTO UserRole VALUES
 
 INSERT INTO Category VALUES
 	(1, 'Revolution'),
-	(2, 'Connasse'),
-	(3, 'Rouleur');
+	(2, 'Professionnalisme'),
+	(3, 'Patin');
 
 INSERT INTO Idea (`date`, description, image, title, author_id, category_id) VALUES
 	(NOW(), 'Elisons Johanna a la presidence de la planete, ca tombe sous le sens.', 'https://www.groupe-imprim.com/wp-content/uploads/2019/09/election-img2.jpg', 'Johanna Presidente', 2, 1),
-	(NOW(), 'Les patins ca coute la blinde. On en offre une paire a tout le monde!!!', 'https://pbs.twimg.com/media/DlwkRQsU4AAa4tU.jpg', 'Patins gratuits', 3, 3),
+	(NOW(), 'Les patins ca coute la blinde. On en offre une paire a tout le monde!!!', 'https://pbs.twimg.com/media/DlwkRQsU4AAa4tU.jpg', 'Patins gratuits', 1, 3),
 	(NOW(), 'Les rendus par mail ca me degoute!', 'https://resize-parismatch.lanmedia.fr/r/620,/img/var/news/storage/images/media/images/super-nanny-portrait/207874-1-fre-FR/Super-Nanny-portrait_original_backup.jpg', '1 point de penalite', 4, 2),
-	(NOW(), 'Java c''etait bien... en 1990', 'https://pic.clubic.com/v1/images/1501424/raw', 'Java', 5, 1),
-	(NOW(), 'Moins de projets à l''EPSI... On a trop de taff', 'https://storage.googleapis.com/prod-phoenix-bucket/osp/cards/534/logo-epsi-200727055421.jpg', 'EPSI', 2, 1);
+	(NOW(), 'Moins de beton, plus de charmes-houblon!!!', 'https://media.ooreka.fr/public/image/plant/2103/mainImage-source-13636621.jpg', 'Nature', 5, 1),
+	(NOW(), 'Moins de projets à l''EPSI... On a trop de taff :(', 'https://storage.googleapis.com/prod-phoenix-bucket/osp/cards/534/logo-epsi-200727055421.jpg', 'EPSI', 3, 1);
 	
